efe9c3167a21d850502ea0e4e956578482e76ec1
Nanthawit <sadzasad747@gmail.com>
2025-05-31 15:32:52 +0700

    basic dart and condision Obarater

---
+++ dart1.dart/dart4.dart
@@ -0,0 +1,9 @@
+// Dynamic variable Type
+void main() {
+  dynamic x = 10.75;
+  print("x = $x");
+  x = "สุดหล่อ";
+  print("x = $x");
+  x = 100;
+  print("3. x = $x");
+}
---
+++ dart1.dart/dart6.dart
@@ -0,0 +1,15 @@
+// class & object
+void main() {
+  // Object
+  var person = Human();
+  print(person.name);
+
+  // Object rewrite value
+  var person2 = new Human();
+  print(person2.name = "Tiger");
+}
+  
+  // Class
+  class Human {
+    String name = "John";
+  }
